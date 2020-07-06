e4c2d4487cd028605ba48a33e6f479f35cf75bd9
Sharon Kim <sharonkim1709@gmail.com>
2020-07-06 17:39:50 -0400

    added get route to return all saved notes

--- server.js
+++ server.js
@@ -18,48 +18,39 @@ app.use(express.static("public"));
 // functionalities
 // view Routes -> html
 // get route
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-// api routes -> JSON
-// to send note data back 
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
-
-// API Routes -> JSON
-app.get("/api/notes", (req, res) => {
-  fs.readFile("./db/db.json", "utf8", (err, data) => {
-    if (err) {
-      return res.send("Error occurred");
-    } else {
-      // convert json to js
-      const arrayOfNotes = JSON.parse(data);
-      // converts back to json
-      res.json(arrayOfNotes);
-    }
-  });
+// reads file and returns all saved notes
+app.get("/api/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "/db/db.json"));
 });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+// API Routes -> JSON
 // post route
 // add new note by post request
 app.post("/api/notes", (req, res) => {
-//     // Read data from the file
+    // Read data from the file
   fs.readFile("./db/db.json", "utf8", (err, data) => {
     if (err) {
       return res.send("Error occurred");
     } else {
-//         // manipulate/update the data
+      // manipulate/update the data
       const arrayOfNotes = JSON.parse(data);
       arrayOfNotes.push(req.body);
-//     // write the data back to file
-    fs.writeFile("./db/db.json", JSON.stringify(arrayOfNotes), "utf8", (err)=>{
-        if(err){
-            return res.send("An error occurred writing your data")
+      // write the data back to file
+      fs.writeFile("./db/db.json",JSON.stringify(arrayOfNotes),(err) => {
+          if (err) {
+            return res.send("An error occurred writing your data");
+          }
+          res.json("Successfully wrote note!");
         }
-        res.json(arrayOfNotes)
-    })
+      );
     }
   });
+
 });
 // delete route
 
