c1be414b150eb08404a8b2298fa2fd38506a22ac
Sharon Kim <sharonkim1709@gmail.com>
2020-07-05 23:12:51 -0400

    added view routes for root and notes.html

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 // Require express
 const express = require("express")
+// path package
+const path = require("path")
 // Create an instance of express
 const app = express()
 // PORT
 const PORT = process.env.PORT || 3000;
+
+// functionalities
+// view Routes -> html
+// get route
+app.get("/", (req,res)=>{
+    res.sendFile(path.join(__dirname,"./public/index.html"))
+})
+
+app.get("/notes",(req,res)=>{
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+})
+
+// API Routes -> JSON
+
+// post route
+// delete route
+
 // Listener
 app.listen(PORT, ()=>{
 console.log(`listening on http://localhost:${PORT}`)
